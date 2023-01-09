346f6b0eeee2c25ce2f8742e89af8829465c96c1
Daniyal <daniyalkhalil28@gmail.com>
2023-01-09 22:33:21 +0500

    dosr

--- test.js
+++ test.js
@@ -1,5 +1,5 @@
 const webdriver = require('selenium-webdriver');
-const firefox = require('selenium-webdriver/firefox');
+const chrome = require('selenium-webdriver/chrome');
 const { By } = require('selenium-webdriver');
 
 function wait(milliseconds) {
@@ -9,8 +9,8 @@ function wait(milliseconds) {
 }
 const test = async () => {
     const driver = new webdriver.Builder()
-        .forBrowser('firefox')
-        .setFirefoxOptions(new firefox.Options().headless())
+        .forBrowser('chrome')
+        .setChromeOptions(new chrome.Options().headless())
         .build();
     console.log('Loading Webpage');
     await driver.get(`http://localhost:3000`);
@@ -18,15 +18,15 @@ const test = async () => {
     console.log('1) Testing If app loads');
     const title = await driver.getTitle();
     // console.log(title == "Node To Do Application");
-    console.log(title == 'Todo');
+    console.log(title == 'items');
 
-    console.log('2) Checking no pending task');
+    console.log('2) Checking no pending item');
     let initial_task = await driver.findElements(By.css('.taskrows'));
     console.log(initial_task.length == 0);
 
-    console.log('3) Add new tasks');
+    console.log('3) Add new item');
     let added_tasksUpdated1 = await driver.findElements(By.css('.taskrows'));
-    await driver.findElement(By.css('#input')).sendKeys('Input added');
+    await driver.findElement(By.css('#input')).sendKeys('milk');
     await driver.findElement(By.css('#addbtn')).click();
     await wait(2000);
     let added_tasksUpdated2 = await driver.findElements(By.css('.taskrows'));
@@ -34,7 +34,7 @@ const test = async () => {
     // console.log(added_tasksUpdated2.length);
     console.log(added_tasksUpdated2.length == added_tasksUpdated1.length + 1);
 
-    console.log('4) Delete new tasks');
+    console.log('4) Delete new item');
     let deletebuttons = await driver.findElements(By.css('.delete-button'));
     await deletebuttons[deletebuttons.length - 1].click();
     // await driver.findElement(By.css('#input')).sendKeys('Input added');
@@ -45,7 +45,7 @@ const test = async () => {
     // console.log(added_tasksUpdated2.length);
     console.log(deleted_tasksUpdated3.length == added_tasksUpdated2.length - 1);
 
-    console.log('5) Checking deleting tasks');
+    console.log('5) Checking deleting item');
     let final_task_delete = await driver.findElements(By.css('.taskrows'));
     console.log(final_task_delete.length == 0);
 };
